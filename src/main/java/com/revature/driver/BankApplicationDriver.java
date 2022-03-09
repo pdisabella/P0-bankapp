b6c347f658d6d8e6007d334c4bc450bc4c40891c
pdisabella <99217905+pdisabella@users.noreply.github.com>
2022-03-09 13:22:05 -0500

    Add files via upload

--- src/main/java/com/revature/driver/BankApplicationDriver.java
+++ src/main/java/com/revature/driver/BankApplicationDriver.java
@@ -1,12 +1,42 @@
 package com.revature.driver;
-
+import java.util.Scanner;
 /**
  * This is the entry point to the application
  */
 public class BankApplicationDriver {
-
+	final static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		// your code here...
+		String home = "H";
+		System.out.println("Hello User");
+		while(home.equals("H")) {			
+			System.out.println("Enter Input Code:");
+			System.out.println("0 = Directory")
+			String input = sc.nextLine();
+			switch(input) {
+			case "0":
+				System.out.println("1 = Create Account");
+				System.out.println("2 = Customer Login");
+				System.out.println("3 = Employee login");
+				break;
+			case "1":
+				CustomerDriver.create();
+				break;
+			case "2":
+				CustomerDriver.login();
+				break;
+			case "3":
+				EmployeeDriver.login();
+				break;
+			default:
+				System.out.println("Invalid Input");
+			}
+			
+			System.out.println("H = Start from home");
+			home = sc.nextLine();
+		}
+	}
+}
+		
 	}
 
 }
