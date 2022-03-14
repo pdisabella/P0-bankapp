f1cd03a1ef1d9d7d56c11b4c4441291fbd4bbbeb
pdisabella <99217905+pdisabella@users.noreply.github.com>
2022-03-14 09:52:36 -0400

    Add files via upload

--- src/main/java/com/revature/utils/ConnectionUtil.java
+++ src/main/java/com/revature/utils/ConnectionUtil.java
@@ -2,33 +2,58 @@ package com.revature.utils;
 
 import java.sql.Connection;
 import java.util.Properties;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 /**
  * Singleton utility for creating and retrieving database connection
  */
 public class ConnectionUtil {
-	private static ConnectionUtil cu = null;
-	private static Properties prop;
-	
-	/**
-	 * This method should read in the "database.properties" file and load
-	 * the values into the Properties variable
-	 */
-	private ConnectionUtil() {
-	}
-	
-	public static synchronized ConnectionUtil getConnectionUtil() {
-		if(cu == null)
-			cu = new ConnectionUtil();
-		return cu;
-	}
-	
-	/**
-	 * This method should create and return a Connection object
-	 * @return a Connection to the database
-	 */
-	public Connection getConnection() {
-		// Hint: use the Properties variable to setup your Connection object
+	private static Connection conn = null;
+
+	// Define a method to get the connection
+
+	public static Connection getConnection() {
+
+		try {
+
+			// Check if a connection doesn't exist - if so then make one
+
+			if (conn == null) {
+
+				// 'hot-fix' to ensure that the driver loads correctly when our application starts
+
+				Class.forName("org.postgresql.Driver"); 
+				
+				// more secure way to input our database credentials
+				
+				Properties props = new Properties();
+				
+				InputStream input = ConnectionUtil.class.getClassLoader().getResourceAsStream("connection.properties");
+				
+				props.load(input);
+				
+				String url = props.getProperty("url");
+				String username = props.getProperty("username");
+				String password = props.getProperty("password");
+
+				conn = DriverManager.getConnection(url, username, password);
+				
+				return conn;
+				
+			} else {
+				
+				return conn;
+			}
+
+		} catch (SQLException | IOException | ClassNotFoundException e) {
+
+			e.printStackTrace();
+		}
+
 		return null;
 	}
 }
